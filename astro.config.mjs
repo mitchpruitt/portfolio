5e19184e832649a91b8912ff072f87828864cb55
Mitch Pruitt <mitch@mitchpruitt.com>
2024-08-26 14:24:01 -0400

    Config for favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import favicons from "astro-favicons";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+    integrations: [
+        favicons({
+          masterPicture: "./src/favicon.svg",
+          emitAssets: true,
+          faviconsDarkMode: true,
+        }),
+      ]
+});
