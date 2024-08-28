35b25658ef527c20e764cafc5112dd25146ebf9b
Mitch Pruitt <mitch@mitchpruitt.com>
2024-08-27 21:52:25 -0400

    Adds MDX capabilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import favicons from "astro-favicons";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-
-    integrations: [
-        favicons({
-          masterPicture: "./src/favicon.svg",
-          emitAssets: true,
-          faviconsDarkMode: true,
-        }),
-      ]
+  integrations: [favicons({
+    masterPicture: "./src/favicon.svg",
+    emitAssets: true,
+    faviconsDarkMode: true
+  }), mdx()]
 });
