69c3cc136fe5bee53b8e4af8cd5163a596e09dbb
Mitch Pruitt <mitch@mitchpruitt.com>
2024-09-07 20:07:36 -0400

    Lenis plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import favicons from "astro-favicons";
 import { astroImageTools } from "astro-imagetools";
 import mdx from "@astrojs/mdx";
 
+import lenis from "astro-lenis";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    favicons({
+  integrations: [favicons({
     masterPicture: "./src/favicon.svg",
     emitAssets: true,
     faviconsDarkMode: true
-  }), 
-    mdx(),
-    astroImageTools
-  ]
+  }), mdx(), astroImageTools, lenis()]
 });
