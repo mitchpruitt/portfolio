842db45857d77fb83addf25d17ab5fb8b4588369
Mitch Pruitt <mitch@mitchpruitt.com>
2024-09-20 23:38:15 -0400

    Forces trailing slashes to mimic Netlify deploy settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-import favicons from "astro-favicons";
-import { astroImageTools } from "astro-imagetools";
-import mdx from "@astrojs/mdx";
+import favicons from 'astro-favicons';
+import { astroImageTools } from 'astro-imagetools';
+import mdx from '@astrojs/mdx';
 
-import lenis from "astro-lenis";
+import lenis from 'astro-lenis';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mitchpruitt.com',
+  trailingSlash: 'always',
   integrations: [favicons({
     masterPicture: "./src/favicon.svg",
     emitAssets: true,
