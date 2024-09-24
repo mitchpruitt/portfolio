a8ee0d0748889a2926c96081d390658103445c50
Mitch Pruitt <mitch@mitchpruitt.com>
2024-09-24 17:02:42 -0400

    Adds sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import favicons from 'astro-favicons';
 import { astroImageTools } from 'astro-imagetools';
 import mdx from '@astrojs/mdx';
-
 import lenis from 'astro-lenis';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +14,5 @@ export default defineConfig({
     masterPicture: "./src/favicon.svg",
     emitAssets: true,
     faviconsDarkMode: true
-  }), mdx(), astroImageTools, lenis()]
+  }), mdx(), astroImageTools, lenis(), sitemap()]
 });
